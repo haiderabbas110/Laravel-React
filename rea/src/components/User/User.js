9a80639b6ba7241cb6bf433e9914bdfea9f53d9f
haider.abbas <haider.abbas@genetechsolutions.com>
2021-11-30 22:48:30 +0500

    user localstorage

--- rea/src/components/User/User.js
+++ rea/src/components/User/User.js
@@ -2,15 +2,24 @@ import { path_server, request_delay } from "../../Constants";
 import React,{Component,useState, useEffect} from "react"
 import ReactDOM from "react-dom"
 import Form from "react-bootstrap/Form";
+import { useHistory } from "react-router-dom";
+
 import Button from "react-bootstrap/Button";
 
 function User(){
     const axios = require('axios');
+    const userToke =   localStorage.getItem("userToken");
+    const history = useHistory();
 
+    if(!userToke){
+      this.history.pushState(null, 'login');
+
+
+    }
       axios.get(path_server+"/api/users")
       .then(res => {
         if(res){
-            debugger
+     
         }
 
       });
